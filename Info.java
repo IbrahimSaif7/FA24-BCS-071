c643c8fc6c5c8b34668bb964b2e9a74525477f30
Muhammad Ibrahim Saif <140160246+IbrahimSaif7@users.noreply.github.com>
2025-02-15 14:40:10 +0500

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,17 @@
+public class Info{
+
+     public static void main(String args[]){
+
+
+             System.out.println("NAME: Ibrahim Saif");
+             System.out.println("AGE: 20");  
+             System.out.print("CLASS: BCS-B");  
+        
+      
+
+
+       }
+
+
+
+  }
